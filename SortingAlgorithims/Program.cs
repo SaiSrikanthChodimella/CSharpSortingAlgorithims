f525f9d59e8a3f2ef442ac34e705aa1e2928e4a9
SaiSrikanthChodimella <srikanth.chodimella@gmail.com>
2025-02-13 23:38:10 +0100

    Removed comments from Main Method

--- SortingAlgorithims/Program.cs
+++ SortingAlgorithims/Program.cs
@@ -1,21 +1,9 @@
 ﻿using BenchmarkDotNet.Running;
-using SortingAlgorithms;
-
 
 namespace SortingAlgorithims
 {
     public class Program
     {
-        static void Main(string[] args)
-        {
-
-            //var randomNumbersArray = new RandomArrayGenerator();
-            //var _unSortedArray = randomNumbersArray.GenerateArrayOfRandomNumbers(500_000);
-
-            //var sortedArray = ParallelMergeSort.SortArrayParallelMergeSort(_unSortedArray);
-
-
-            BenchmarkRunner.Run<BenchMarkAlgorithims>();
-        }
+        static void Main(string[] args) => BenchmarkRunner.Run<BenchMarkAlgorithims>();
     }
 }
